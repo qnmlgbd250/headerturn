82e8453a760b902d4621710723b8db2a642bf1ad
if <zcshiyonghao@163.com>
2022-06-19 13:47:29 +0800

    Revert "加载模板路径调整"
    This reverts commit 4026ee2e

--- main.py
+++ main.py
@@ -6,21 +6,27 @@
 # @Software: PyCharm
 import re
 import json
-from fastapi import FastAPI,Form,Request
+import rsa, base64
+from fastapi import FastAPI, Form, Request
 import uvicorn
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
+app.mount("/static", StaticFiles(directory = "static"), name = "static")
+
+
 @app.get("/h")
 def getdate(request: Request):
-    return templates.TemplateResponse('header_before.html',context = {'request':request})
+    return templates.TemplateResponse('header_before.html', context = {'request': request})
 
 
 @app.post("/h")
-def turn(request: Request,data: str = Form(...)):
+def turn(request: Request, data: str = Form(...)):
     s = f'''{data}'''
     pattern = '^(.*?): (.*?)$'
     output = '{'
@@ -31,15 +37,16 @@ def turn(request: Request,data: str = Form(...)):
         output += '\n'
     output += "}"
     output = output.replace(",}", '}')
-    return templates.TemplateResponse('output.html',context = {'request':request,'output':output})
+    return templates.TemplateResponse('output.html', context = {'request': request, 'output': output})
+
 
 @app.get("/c")
 def getdate(request: Request):
-    return templates.TemplateResponse('cookie_before.html',context = {'request':request})
+    return templates.TemplateResponse('cookie_before.html', context = {'request': request})
 
 
 @app.post("/c")
-def turn(request: Request,data: str = Form(...)):
+def turn(request: Request, data: str = Form(...)):
     s = f'''{data}'''
     pattern = '^(.*?)=(.*?)$'
     output = '{'
@@ -50,22 +57,49 @@ def turn(request: Request,data: str = Form(...)):
         output += '\n'
     output += "}"
     output = output.replace(",}", '}')
-    return templates.TemplateResponse('output.html',context = {'request':request,'output':output})
+    return templates.TemplateResponse('output.html', context = {'request': request, 'output': output})
+
 
 @app.get("/u")
 def getdate(request: Request):
-    return templates.TemplateResponse('unicode_before.html',context = {'request':request})
+    return templates.TemplateResponse('unicode_before.html', context = {'request': request})
 
 
 @app.post("/u")
-def turn(request: Request,data: str = Form(...)):
+def turn(request: Request, data: str = Form(...)):
     try:
         data = data.replace(r'\\', '\\')
         s = r'{}'.format(data)
         output = json.loads(f'"{s}"')
-        return templates.TemplateResponse('output.html',context = {'request':request,'output':output})
+        return templates.TemplateResponse('output.html', context = {'request': request, 'output': output})
     except:
-        return templates.TemplateResponse('output.html',context = {'request':request,'output':'输入错误'})
+        return templates.TemplateResponse('output.html', context = {'request': request, 'output': '输入错误'})
+
+
+@app.get("/p")
+def getdate(request: Request):
+    return templates.TemplateResponse('pwd_before.html', context = {'request': request})
+
+
+@app.post("/p")
+def turn(request: Request, data: str = Form(...)):
+    _PRIVATE_KEY_PATH = 'static/rsa_private_key.pem'
+    # privkey 直接放到内存
+    with open(_PRIVATE_KEY_PATH, mode = 'rb') as f:
+        _privkey = rsa.PrivateKey.load_pkcs1(f.read())
+    try:
+        data = data.replace(' ', '+')
+        try:
+            if type(data) == str: data = base64.b64decode(data)
+            message = rsa.decrypt(data, _privkey)
+        except rsa.pkcs1.DecryptionError:
+            print('[error]: Decryption failed')
+            output = ''
+        else:
+            output = message.decode('utf-8')
+        return templates.TemplateResponse('output.html', context = {'request': request, 'output': output})
+    except:
+        return templates.TemplateResponse('output.html', context = {'request': request, 'output': '输入错误'})
 
 
 if __name__ == '__main__':
