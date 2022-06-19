decb2cce61b378740e167b274a13a04a3fc6048f
if <zcshiyonghao@163.com>
2022-06-19 14:10:55 +0800

    测试模板渲染

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ from fastapi import FastAPI, Form, Request
 import uvicorn
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-templates = Jinja2Templates(directory = "templates")
+
 app.mount("/static", StaticFiles(directory = "static"), name = "static")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/h")
+@app.get("/h" ,response_class=HTMLResponse)
 def getdate(request: Request):
     return templates.TemplateResponse('header_before.html', context = {'request': request})
 
