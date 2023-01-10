ad57de1770604c359a3a8ad12640951eb4c86c31
if <zcshiyonghao@163.com>
2023-01-10 09:53:04 +0800

    defaultRule参数过滤老地区

--- main.py
+++ main.py
@@ -109,6 +109,7 @@ def getdate(request: Request):
 @app.post("/d")
 def turn(request: Request, data: str = Form(...)):
     try:
+        old_region_list = ['shanxi', 'shenzhen', "henan", 'guangdong', 'anhui', 'jiangsu']
         data = data.strip()
         rule_json = 'static/rule.json'
         with open(rule_json, mode='rb') as f:
@@ -126,7 +127,7 @@ def turn(request: Request, data: str = Form(...)):
         if data.startswith('1'):
 
             resp = requests.get('https://mtax.kdzwy.com/taxtask/api/task/history', params=param, proxies=proxies).json()
-            if not resp['data'].get('defaultRule'):
+            if not resp['data'].get('defaultRule') and resp['data']['region'] not in old_region_list:
                 resp['data']['defaultRule'] = rule_json_list
             if resp.get('code') in [301]:
                 resp = requests.get('https://test1.kdzwy.com/taxtask/api/task/history', params=param, proxies=proxies).json()
